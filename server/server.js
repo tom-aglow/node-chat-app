abfa2c96e649bcbf8134d9db6fd894a920935321
tom-aglow <tom@ohhhh.me>
2017-11-18 19:06:52 -0800

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -9,23 +9,35 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
 
-
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('hi there');
-  
-  socket.on('createMessage', (msg) => {
+
+  //  GREETING MESSAGES
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'welcome to the chart app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
+
+  //  NEW MESSAGE FROM USER
+  socket.on('createMessage', msg => {
     console.log('create message: ', msg);
     io.emit('newMessage', {
       ...msg,
       createdAt: new Date().getTime()
-    })
-  })
-  
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('bye client');
-  })
+  });
 });
 
 server.listen(port, () => {
